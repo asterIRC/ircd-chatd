5c3ac48f6d3b3506c7b62970869c953390718810
Jaffacake <jaffacake@life.umbrellix.tk>
2014-06-20 11:25:46 -0700

    Wtf? SOP != SUPEROP WTF WTF WTF WRONG WRONG WRONG FUCK FUCK FUCK MY IRCD IS ABOUT AS SANE AS A METH ADDICT ON CRACK -- sorry nenolod

--- modules/core/m_join.c
+++ modules/core/m_join.c
@@ -913,15 +913,369 @@ ms_sjoin(struct Client *client_p, struct Client *source_p, int parc, const char 
 			joins++;
 		}
 
-		if(fl & CHFL_CHANOP)
+		// This makes dogs whimper.
+                if(fl & CHFL_QOP)
+                {
+                        *mbuf++ = 'w';
+                        para[pargs++] = target_p->name;
+
+                        if(fl & CHFL_BOP)
+                        {
+                                /* its possible the +y has filled up MAXMODEPARAMS, if so, start
+                                 * a new buffer
+                                 */
+                                if(pargs >= MAXMODEPARAMS)
+                                {
+                                        *mbuf = '\0';
+                                        sendto_channel_local(ALL_MEMBERS, chptr,
+                                                        ":%s MODE %s %s %s %s %s %s",
+                                                        fakesource_p->name, chptr->chname,
+                                                        modebuf,
+                                                        para[0], para[1], para[2], para[3]);
+                                        mbuf = modebuf;
+                                        *mbuf++ = '+';
+                                        para[0] = para[1] = para[2] = para[3] = NULL;
+                                        pargs = 0;
+                                }
+
+                                *mbuf++ = 'W';
+                                para[pargs++] = target_p->name;
+                        }
+                        if(fl & CHFL_SOP)
+                        {
+                                /* its possible the +y has filled up MAXMODEPARAMS, if so, start
+                                 * a new buffer
+                                 */
+                                if(pargs >= MAXMODEPARAMS)
+                                {
+                                        *mbuf = '\0';
+                                        sendto_channel_local(ALL_MEMBERS, chptr,
+                                                        ":%s MODE %s %s %s %s %s %s",
+                                                        fakesource_p->name, chptr->chname,
+                                                        modebuf,
+                                                        para[0], para[1], para[2], para[3]);
+                                        mbuf = modebuf;
+                                        *mbuf++ = '+';
+                                        para[0] = para[1] = para[2] = para[3] = NULL;
+                                        pargs = 0;
+                                }
+
+                                *mbuf++ = 'a';
+                                para[pargs++] = target_p->name;
+                        }
+                        if(fl & CHFL_CHANOP)
+                        {
+                               /* its possible the +q has filled up MAXMODEPARAMS, if so, start
+                                * a new buffer
+                                */
+                                if(pargs >= MAXMODEPARAMS)
+                                {
+                                        *mbuf = '\0';
+                                        sendto_channel_local(ALL_MEMBERS, chptr,
+                                                     ":%s MODE %s %s %s %s %s %s",
+                                                     fakesource_p->name, chptr->chname,
+                                                     modebuf,
+                                                     para[0], para[1], para[2], para[3]);
+                                        mbuf = modebuf;
+                                        *mbuf++ = '+';
+                                        para[0] = para[1] = para[2] = para[3] = NULL;
+                                        pargs = 0;
+                                }
+                        
+                                *mbuf++ = 'o';
+                                para[pargs++] = target_p->name;
+                        }
+                        if(fl & CHFL_HALFOP)
+                        {
+                                /* its possible the +q has filled up MAXMODEPARAMS, if so, start
+                                 * a new buffer
+                                 */
+                                if(pargs >= MAXMODEPARAMS)
+                                {
+                                        *mbuf = '\0';
+                                        sendto_channel_local(ALL_MEMBERS, chptr,
+                                                     ":%s MODE %s %s %s %s %s %s",
+                                                     fakesource_p->name, chptr->chname,
+                                                     modebuf,
+                                                     para[0], para[1], para[2], para[3]);
+                                        mbuf = modebuf;
+                                        *mbuf++ = '+';
+                                        para[0] = para[1] = para[2] = para[3] = NULL;
+                                        pargs = 0;
+                                }
+
+                                *mbuf++ = 'h';
+                                para[pargs++] = target_p->name;
+                        }
+                        if(fl & CHFL_VOICE)
+                        {
+                                /* its possible the +q has filled up MAXMODEPARAMS, if so, start
+                                 * a new buffer
+                                 */
+                                if(pargs >= MAXMODEPARAMS)
+                                {
+                                        *mbuf = '\0';
+                                        sendto_channel_local(ALL_MEMBERS, chptr,
+                                                     ":%s MODE %s %s %s %s %s %s",
+                                                     fakesource_p->name, chptr->chname,
+                                                     modebuf,
+                                                     para[0], para[1], para[2], para[3]);
+                                mbuf = modebuf;
+                                *mbuf++ = '+';
+                                para[0] = para[1] = para[2] = para[3] = NULL;
+                                pargs = 0;
+                                }
+
+                                *mbuf++ = 'v';
+                                para[pargs++] = target_p->name;
+                        }
+                }
+                else if(fl & CHFL_BOP)
+                {
+                        *mbuf++ = 'W';
+                        para[pargs++] = target_p->name;
+
+                        if(fl & CHFL_QOP)
+                        {
+                                /* its possible the +y has filled up MAXMODEPARAMS, if so, start
+                                 * a new buffer
+                                 */
+                                if(pargs >= MAXMODEPARAMS)
+                                {
+                                        *mbuf = '\0';
+                                        sendto_channel_local(ALL_MEMBERS, chptr,
+                                                        ":%s MODE %s %s %s %s %s %s",
+                                                        fakesource_p->name, chptr->chname,
+                                                        modebuf,
+                                                        para[0], para[1], para[2], para[3]);
+                                        mbuf = modebuf;
+                                        *mbuf++ = '+';
+                                        para[0] = para[1] = para[2] = para[3] = NULL;
+                                        pargs = 0;
+                                }
+
+                                *mbuf++ = 'w';
+                                para[pargs++] = target_p->name;
+                        }
+                        if(fl & CHFL_SOP)
+                        {
+                                /* its possible the +y has filled up MAXMODEPARAMS, if so, start
+                                 * a new buffer
+                                 */
+                                if(pargs >= MAXMODEPARAMS)
+                                {
+                                        *mbuf = '\0';
+                                        sendto_channel_local(ALL_MEMBERS, chptr,
+                                                        ":%s MODE %s %s %s %s %s %s",
+                                                        fakesource_p->name, chptr->chname,
+                                                        modebuf,
+                                                        para[0], para[1], para[2], para[3]);
+                                        mbuf = modebuf;
+                                        *mbuf++ = '+';
+                                        para[0] = para[1] = para[2] = para[3] = NULL;
+                                        pargs = 0;
+                                }
+
+                                *mbuf++ = 'a';
+                                para[pargs++] = target_p->name;
+                        }
+                        if(fl & CHFL_CHANOP)
+                        {
+                               /* its possible the +q has filled up MAXMODEPARAMS, if so, start
+                                * a new buffer
+                                */
+                                if(pargs >= MAXMODEPARAMS)
+                                {
+                                        *mbuf = '\0';
+                                        sendto_channel_local(ALL_MEMBERS, chptr,
+                                                     ":%s MODE %s %s %s %s %s %s",
+                                                     fakesource_p->name, chptr->chname,
+                                                     modebuf,
+                                                     para[0], para[1], para[2], para[3]);
+                                        mbuf = modebuf;
+                                        *mbuf++ = '+';
+                                        para[0] = para[1] = para[2] = para[3] = NULL;
+                                        pargs = 0;
+                                }
+                        
+                                *mbuf++ = 'o';
+                                para[pargs++] = target_p->name;
+                        }
+                        if(fl & CHFL_HALFOP)
+                        {
+                                /* its possible the +q has filled up MAXMODEPARAMS, if so, start
+                                 * a new buffer
+                                 */
+                                if(pargs >= MAXMODEPARAMS)
+                                {
+                                        *mbuf = '\0';
+                                        sendto_channel_local(ALL_MEMBERS, chptr,
+                                                     ":%s MODE %s %s %s %s %s %s",
+                                                     fakesource_p->name, chptr->chname,
+                                                     modebuf,
+                                                     para[0], para[1], para[2], para[3]);
+                                        mbuf = modebuf;
+                                        *mbuf++ = '+';
+                                        para[0] = para[1] = para[2] = para[3] = NULL;
+                                        pargs = 0;
+                                }
+
+                                *mbuf++ = 'h';
+                                para[pargs++] = target_p->name;
+                        }
+                        if(fl & CHFL_VOICE)
+                        {
+                                /* its possible the +q has filled up MAXMODEPARAMS, if so, start
+                                 * a new buffer
+                                 */
+                                if(pargs >= MAXMODEPARAMS)
+                                {
+                                        *mbuf = '\0';
+                                        sendto_channel_local(ALL_MEMBERS, chptr,
+                                                     ":%s MODE %s %s %s %s %s %s",
+                                                     fakesource_p->name, chptr->chname,
+                                                     modebuf,
+                                                     para[0], para[1], para[2], para[3]);
+                                mbuf = modebuf;
+                                *mbuf++ = '+';
+                                para[0] = para[1] = para[2] = para[3] = NULL;
+                                pargs = 0;
+                                }
+
+                                *mbuf++ = 'v';
+                                para[pargs++] = target_p->name;
+                        }
+                }
+                else if(fl & CHFL_SOP)
+		{
+			*mbuf++ = 'a';
+			para[pargs++] = target_p->name;
+
+			if(fl & CHFL_CHANOP)
+			{
+				/* its possible the +a has filled up MAXMODEPARAMS, if so, start
+				 * a new buffer
+				 */
+				if(pargs >= MAXMODEPARAMS)
+				{
+					*mbuf = '\0';
+					sendto_channel_local(ALL_MEMBERS, chptr,
+							     ":%s MODE %s %s %s %s %s %s",
+							     fakesource_p->name, chptr->chname,
+							     modebuf,
+							     para[0], para[1], para[2], para[3]);
+					mbuf = modebuf;
+					*mbuf++ = '+';
+					para[0] = para[1] = para[2] = para[3] = NULL;
+					pargs = 0;
+				}
+
+				*mbuf++ = 'o';
+				para[pargs++] = target_p->name;
+			}
+			if(fl & CHFL_HALFOP)
+			{
+				/* its possible the +a has filled up MAXMODEPARAMS, if so, start
+				 * a new buffer
+				 */
+				if(pargs >= MAXMODEPARAMS)
+				{
+					*mbuf = '\0';
+					sendto_channel_local(ALL_MEMBERS, chptr,
+							     ":%s MODE %s %s %s %s %s %s",
+							     fakesource_p->name, chptr->chname,
+							     modebuf,
+							     para[0], para[1], para[2], para[3]);
+					mbuf = modebuf;
+					*mbuf++ = '+';
+					para[0] = para[1] = para[2] = para[3] = NULL;
+					pargs = 0;
+				}
+
+				*mbuf++ = 'h';
+				para[pargs++] = target_p->name;
+			}
+			if(fl & CHFL_VOICE)
+			{
+				/* its possible the +a has filled up MAXMODEPARAMS, if so, start
+				 * a new buffer
+				 */
+				if(pargs >= MAXMODEPARAMS)
+				{
+					*mbuf = '\0';
+					sendto_channel_local(ALL_MEMBERS, chptr,
+							     ":%s MODE %s %s %s %s %s %s",
+							     fakesource_p->name, chptr->chname,
+							     modebuf,
+							     para[0], para[1], para[2], para[3]);
+					mbuf = modebuf;
+					*mbuf++ = '+';
+					para[0] = para[1] = para[2] = para[3] = NULL;
+					pargs = 0;
+				}
+
+				*mbuf++ = 'v';
+				para[pargs++] = target_p->name;
+			}
+		}
+		else if(fl & CHFL_CHANOP)
 		{
 			*mbuf++ = 'o';
 			para[pargs++] = target_p->name;
 
-			/* a +ov user.. bleh */
+			if(fl & CHFL_HALFOP)
+			{
+				/* its possible the +o has filled up MAXMODEPARAMS, if so, start
+				 * a new buffer
+				 */
+				if(pargs >= MAXMODEPARAMS)
+				{
+					*mbuf = '\0';
+					sendto_channel_local(ALL_MEMBERS, chptr,
+							     ":%s MODE %s %s %s %s %s %s",
+							     fakesource_p->name, chptr->chname,
+							     modebuf,
+							     para[0], para[1], para[2], para[3]);
+					mbuf = modebuf;
+					*mbuf++ = '+';
+					para[0] = para[1] = para[2] = para[3] = NULL;
+					pargs = 0;
+				}
+
+				*mbuf++ = 'h';
+				para[pargs++] = target_p->name;
+			}
 			if(fl & CHFL_VOICE)
 			{
 				/* its possible the +o has filled up MAXMODEPARAMS, if so, start
+				 * a new buffer
+				 */
+				if(pargs >= MAXMODEPARAMS)
+				{
+					*mbuf = '\0';
+					sendto_channel_local(ALL_MEMBERS, chptr,
+							     ":%s MODE %s %s %s %s %s %s",
+							     fakesource_p->name, chptr->chname,
+							     modebuf,
+							     para[0], para[1], para[2], para[3]);
+					mbuf = modebuf;
+					*mbuf++ = '+';
+					para[0] = para[1] = para[2] = para[3] = NULL;
+					pargs = 0;
+				}
+
+				*mbuf++ = 'v';
+				para[pargs++] = target_p->name;
+			}
+		}
+		else if(fl & CHFL_HALFOP)
+		{
+			*mbuf++ = 'h';
+			para[pargs++] = target_p->name;
+
+			if(fl & CHFL_VOICE)
+			{
+				/* its possible the +h has filled up MAXMODEPARAMS, if so, start
 				 * a new buffer
 				 */
 				if(pargs >= MAXMODEPARAMS)
