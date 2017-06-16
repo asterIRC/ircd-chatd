7fa327c6e80a043c123ad28897dca3ca5f89a6aa
L <j4jackj@gmx.com>
2017-06-15 18:44:12 -0700

    extb_ssl_fp: in preparation for merger to charybdis mainline, reformat code

--- extensions/extb_ssl_fp.c
+++ extensions/extb_ssl_fp.c
@@ -1,4 +1,7 @@
 /* SSL extban type: matches ssl users */
+/* This file is available under the same conditions as the rest of
+   https://github.com/asterIRC/ircd-chatd, and by extension, the rest
+   of Charybdis. */
 
 #include "stdinc.h"
 #include "modules.h"
@@ -9,7 +12,7 @@ static int _modinit(void);
 static void _moddeinit(void);
 static int eb_ssl(const char *data, struct Client *client_p, struct Channel *chptr, long mode_type);
 
-DECLARE_MODULE_AV1(extb_ssl, _modinit, _moddeinit, NULL, NULL, NULL, "$Revision$");
+DECLARE_MODULE_AV1(extb_ssl, _modinit, _moddeinit, NULL, NULL, NULL, "1.05");
 
 static int
 _modinit(void)
@@ -26,14 +29,15 @@ _moddeinit(void)
 }
 
 static int eb_ssl(const char *data, struct Client *client_p,
-		struct Channel *chptr, long mode_type)
+                  struct Channel *chptr, long mode_type)
 {
 
 	(void)chptr;
 	(void)mode_type;
 	if (data != NULL)
 	{
-		if (EmptyString(client_p->certfp)) return EXTBAN_NOMATCH;
+		if (EmptyString(client_p->certfp))
+			return EXTBAN_NOMATCH;
 		return !irccmp(data, client_p->certfp) ? EXTBAN_MATCH : EXTBAN_NOMATCH;
 	}
 	return IsSSLClient(client_p) ? EXTBAN_MATCH : EXTBAN_NOMATCH;
