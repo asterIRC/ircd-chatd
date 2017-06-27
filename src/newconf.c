973c226de7eeadaa7a2e970a2ce95884a86d9d2c
Ellenor Malik <ellenor@umbrellix.net>
2017-06-26 21:26:42 -0700

    Ugh

--- src/newconf.c
+++ src/newconf.c
@@ -1044,7 +1044,7 @@ conf_end_auth(struct TopConf *tc)
 			(!found_conf->spasswd || (yy_aconf->spasswd &&
 			    0 == irccmp(found_conf->spasswd, yy_aconf->spasswd))))
 		conf_report_error("Ignoring duplicate auth block for %s@%s[%s]",
-				yy_aconf->user, yy_aconf->host, (yy_aconf->name2 == NULL) ? "no webircname" : yy_aconf->name2);
+				yy_aconf->user, yy_aconf->host, (yy_aconf->info.name2 == NULL) ? "no webircname" : yy_aconf->info.name2);
 	else
 		add_conf_by_address(yy_aconf->host, CONF_CLIENT, yy_aconf->user, yy_aconf->spasswd, yy_aconf);
 
